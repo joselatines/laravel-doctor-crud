794c224495292263af52822a3989fcf80914ab1a
Jose Latines <joselatines33@gmail.com>
2024-04-10 14:13:59 -0400

    mutadores y accesores agregados

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,22 @@ class Patient extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'last_name', 'email', 'age', 'note'];
+
+    protected function name(): Attribute {
+
+        return new Attribute(
+            get: fn($value) => ucwords($value),
+            set: function($value) {
+                return strtolower($value);
+            }
+        );
+    }
+
+    protected function last_name(): Attribute {
+        return new Attribute(
+            set: function($value){
+                return strtolower($value);
+            }
+        );
+    }
 }
